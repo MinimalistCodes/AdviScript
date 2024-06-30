e2240ecbc31bb4aa590a411c9e5dec69bfb07669
MinimalistCodes <mike.decavalcante@gmail.com>
2024-06-30 15:30:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,16 @@ import os, sys
 # Load environment variables
 load_dotenv()
 
-# Configure Google Gemini API - Remove this section as we will use langchain
-api_key = os.getenv("GOOGLE_API_KEY")
 
-#load css file
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 local_css("style.css")
+
+
+# Configure Google Gemini API - Remove this section as we will use langchain
+api_key = os.getenv("GOOGLE_API_KEY")
 
 
 # Function to generate the cold call script
