e8029f9451fdf6bbb870e26e57f3a8ba214e9c2a
MinimalistCodes <mike.decavalcante@gmail.com>
2024-06-30 15:10:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ api_key = os.getenv("GOOGLE_API_KEY")
 # Function to generate the cold call script
 def cold_script(industry, keywords, length, tone, scripit_type):
     return f"""
-You are a skilled {industry} scriptwriter.
-
 Please generate a {scripit_type} script for a {industry} company that specializes in {keywords}.
 The script should be tailored to a {tone} tone and a {length} length. 
 Include a structured call-flow, handle objections, and provide rebuttals both implied and explicitly handled within the script. 
@@ -49,26 +47,23 @@ for message in st.session_state.messages:
 with st.form("input_form"):
     form_choice = st.selectbox(
         "Select Industry:",
-        ["Technology", "Finance", "Healthcare", "Education", "Sales", "Marketing", "Other"]
-    )
+        ["Technology", "Healthcare", "Finance", "Manufacturing", "Retail", "Professional Services", "Real Estate", "Marketing", "Legal", "Automotive", "Construction", "Entertainment", "Education", "Hospitality"]
 
-    if form_choice == "Other":
-        other_industry = st.text_input("Please specify the industry:")
-        industry = other_industry if other_industry else form_choice
-    else:
-        industry = form_choice
+    )
+    
+    industry = form_choice
 
 # Form script type
-    form_script_type = st.selectbox("Select Script Type:", ["Cold Call", "Follow-up Call", "Voicemail", "Sales Email", "Other"])
+    form_script_type = st.selectbox("Select Script Type:", ["Discovery Calls", ",Cold Calls", ",Elevator Pitches","Remote Selling Scripts","Product Demo Scripts","Objection Handling Scripts", "Negotiation Scripts", ",Referral Scripts", ",Customer Storytelling Scripts"])
 
     form_tone = st.selectbox("Select Tone:", ["Professional and Trustworthy", "Casual and conversational", "Persuasive and Assertive", "Empathetic and Supportive", "Energetic and Enthusiastic", "Urgent and persuasive", "Friendly and approachable"])
     form_length = st.selectbox("Select Length:", ["Short", "Medium", "Long"])
     form_keywords = st.text_input("Enter 3 descriptive keywords (comma-separated):")
     
 
-    submitted = st.form_submit_button("Generate Script")
-    if submitted:
-        keywords_list = [keyword.strip() for keyword in form_keywords.split(",")]
-        response = ai_chatbot(industry, form_tone.lower(), form_length.lower(), keywords_list)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+submitted = st.form_submit_button("Generate Script")
+if submitted:
+    keywords_list = [keyword.strip() for keyword in form_keywords.split(",")]
+    response = ai_chatbot(industry, form_tone.lower(), form_length.lower(), keywords_list)
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
