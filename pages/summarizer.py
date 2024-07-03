f0f61e6d8e6e6bb7e26b5d25ff4510d27c3e3c63
MinimalistCodes <mike.decavalcante@gmail.com>
2024-07-02 23:59:16 -0500

    Update summarizer.py

--- pages/summarizer.py
+++ pages/summarizer.py
@@ -1,5 +1,12 @@
+import time
 import streamlit as st
-from langchain.llms import GoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAI
+from dotenv import load_dotenv
+import os, sys, json
+from fpdf import FPDF
+from streamlit_extras.switch_page_button import switch_page
+from streamlit_extras.add_vertical_space import add_vertical_space
+from PIL import Image
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
