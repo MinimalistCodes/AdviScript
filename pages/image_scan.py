fd90742f3b379d63ec9e311e492c84db7f4992c5
MinimalistCodes <mike.decavalcante@gmail.com>
2024-07-03 13:58:39 -0500

    Update image_scan.py

--- pages/image_scan.py
+++ pages/image_scan.py
@@ -1,14 +1,33 @@
-from datetime import datetime
-import time
-
 import streamlit as st
-from langchain_google_genai import GoogleGenerativeAI
-from dotenv import load_dotenv
-import os, sys, json
-from fpdf import FPDF
-from streamlit_extras.switch_page_button import switch_page
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+import io
 from PIL import Image
-from auth_helper import requires_auth
 
+def image_scanner():
+    st.title("Image Scanner")
 
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        image = Image.open(io.BytesIO(bytes_data))
+        st.image(image, caption="Uploaded Image.", use_column_width=True)
+        
+        if prompt := st.text_input("Ask the AI about the image:"):
+            
+            llm = ChatGoogleGenerativeAI(model="gemini-pro-vision")
+            # image_url = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            message = HumanMessage(
+                content=[
+                    {
+                        "type": "text",
+                        "text": prompt,
+                    }, 
+                    {"type": "image", "image": image},
+                ]
+            )
+            response = llm.invoke([message])
+            st.write(response.content)
 
+# Run the image scanner
+image_scanner()
