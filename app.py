8f442d23ebc995c2db20960c4b8cb93024b01166
MinimalistCodes <mike.decavalcante@gmail.com>
2024-07-03 08:11:16 -0500

    Multipage Issues

--- app.py
+++ app.py
@@ -1,31 +1,14 @@
 import streamlit as st
-from langchain_google_genai import GoogleGenerativeAI
-from dotenv import load_dotenv
 import os, sys, json
 from fpdf import FPDF
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.add_vertical_space import add_vertical_space
 from PIL import Image
 
-# Load environment variables
-load_dotenv()
+st.set_page_config(
+    page_title="SalesTrek - Your multi-tool for sales and marketing!",
+    page_icon="📈"
+)
 
-api_key = os.getenv("GOOGLE_API_KEY")
-
-try:
-    from pages import chat_with_coach, sales_script_generator, home, email_generator, summarizer, image_scan, settings
-except ModuleNotFoundError as e:
-    st.error(f"Error importing modules: {e}")
-    
-# Add all your applications (pages) here
-app.add_page("Home", home.app)
-app.add_page("Chat with Coach", chat_with_coach.app)
-app.add_page("Sales Script Generator", sales_script_generator.app)
-app.add_page("Email Generator", email_generator.app)
-app.add_page("Summarizer", summarizer.app)
-app.add_page("Image Scan", image_scan.app)
-app.add_page("Settings", settings.app)
-
-
-# The main app
-app.run()
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
