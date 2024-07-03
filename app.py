96da6b1a5b5797b503af690f8247ef4dd9bacf22
MinimalistCodes <mike.decavalcante@gmail.com>
2024-07-03 08:51:57 -0500

    WIP

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import os, sys, json
 from fpdf import FPDF
 from streamlit_extras.switch_page_button import switch_page
-from streamlit_extras.add_vertical_space import add_vertical_space
 from PIL import Image
 import runtimes as rt
 
--- pages/home.py
+++ pages/home.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 import os, sys, json
 from fpdf import FPDF
 from streamlit_extras.switch_page_button import switch_page
-from streamlit_extras.add_vertical_space import add_vertical_space
 from PIL import Image
 
 
