8e1614bc794c23553aea5d8b03369b98ed0a919b
MinimalistCodes <mike.decavalcante@gmail.com>
2024-07-05 06:03:53 -0500

    Page sum.

--- pages/summarizer.py
+++ pages/summarizer.py
@@ -1,49 +1,27 @@
-import time
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-import streamlit as st
-from langchain_google_genai import GoogleGenerativeAI
+from langchain import PromptTemplate
+from langchain.document_loaders import WebBaseLoader
+from langchain.schema import StrOutputParser
+from langchain.schema.prompt_template import format_document
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.document_loaders import WebBaseLoader
-from langchain.chains import StuffDocumentsChain
-from langchain.chains.llm import LLMChain
-from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
-import os, sys, json
-from fpdf import FPDF
-from streamlit_extras.switch_page_button import switch_page
-
+import streamlit as st
+import os
+    
 # Load environment variables
 
 api_key = os.getenv("GOOGLE_API_KEY")
 
 def summarize_text_or_url(input_value):
-    try:
-        # Load the content
-        if input_value.startswith("http"):  # It's a URL
-            loader = WebBaseLoader(input_value)
-            docs = loader.load()  # Load the web page content
-        else:
-            docs = [{"text": input_value}]  # Treat as plain text
+    llm = ChatGoogleGenerativeAI(model="gemini-pro",
+                 temperature=0.7, top_p=0.85)
+    if input_value.startswith("http"):
+        document = WebBaseLoader().load(input_value)
+    else:
+        document = input_value
+    prompt = PromptTemplate("summarize", document=document)
+    response = llm(prompt)
+    summary = StrOutputParser().parse(response)
+    return summary
 
-        # Initialize LLM and summarization chain
-        llm = GoogleGenerativeAI(temperature=0, GOOGLE_API_KEY=api_key)
-        text_splitter = RecursiveCharacterTextSplitter(
-            separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""], chunk_size=1000, chunk_overlap=200
-        )
-        prompt = PromptTemplate(
-            template="Write a concise summary of the following:\n{text}\nCONCISE SUMMARY:",
-            input_variables=["text"]
-        )
-        llm_chain = LLMChain(llm=llm, prompt=prompt)
-        stuff_chain = StuffDocumentsChain(llm_chain=llm_chain, document_variable_name="text")
-
-        # Invoke the summarization chain
-        response = stuff_chain.run(docs)
-        return response
-    except Exception as e:
-        st.error(f"Error: {e}")
-        return "Sorry, I couldn't process the request. Please check the input or try again later."
 
 # UI Layout
 def app():
